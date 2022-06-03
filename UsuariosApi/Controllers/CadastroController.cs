1c2acf9e7691373852c2bbd0c39ed8304018177d
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-06-03 15:58:33 -0300

    [Progress] 09 Cadastrando um usuário

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastroUsuario(CreateUsuarioDto model) 
+        {
+            //TODO chamar o service
+            return Ok();
+        }
+    }
+}
